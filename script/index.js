2fe289adb3be11a06319480da9f5141ebb6c91f9
Nooshin <2011dragonmails@gmail.com>
2023-02-03 19:47:05 +0330

    loading animation added

--- script/index.js
+++ script/index.js
@@ -3,26 +3,38 @@ const burger = document.querySelector(".nav-toggle");
 const burgerImg = burger.querySelector("img");
 const artList = document.querySelector(".art-list");
 const artTemplate = document.getElementById("art-info");
+const loading = document.querySelector(".loading-container");
+const main = document.querySelector(".main");
 
-
-let artData;
+let artData = {};
 fetch("https://api.artic.edu/api/v1/artworks?page=1&limit=9")
   .then((response) => response.json())
   .then((data) => {
+    loading.classList.add("hidden");
     artData = data.data;
     // console.log(artData[2]);
     // console.log(artData[3]);
+    if (Object.keys(artData).length > 0) {
+      main.classList.remove("hidden");
+    }
     artData.forEach((element) => {
       const artBody = document.importNode(artTemplate.content, true);
-      artBody.querySelector('img').src=`https://www.artic.edu/iiif/2/${element.image_id}/full/843,/0/default.jpg`;
-      artBody.querySelector('.artist').textContent = element.artist_title;
-      artBody.querySelector('.img-desc').textContent = element.title;
-      artBody.querySelector('.likes').textContent = (Math.floor(Math.random() * 100)).toString();
-      artBody.querySelector('.views').textContent = (Math.floor(Math.random() * (1000 - 100) + 100) / 100).toString() + 'k';
+      artBody.querySelector(
+        "img"
+      ).src = `https://www.artic.edu/iiif/2/${element.image_id}/full/843,/0/default.jpg`;
+      artBody.querySelector(".artist").textContent = element.artist_title;
+      artBody.querySelector(".img-desc").textContent = element.title;
+      artBody.querySelector(".likes").textContent = Math.floor(
+        Math.random() * 100
+      ).toString();
+      artBody.querySelector(".views").textContent =
+        (Math.floor(Math.random() * (1000 - 100) + 100) / 100).toString() + "k";
       artList.append(artBody);
     });
   })
-  .catch((error) => console.log(error));
+  .catch((error) => {
+    console.log(error);
+  });
 
 const mobileNavHandler = (e) => {
   const navItem = e.target.tagName === "H3" ? e.target : null;
