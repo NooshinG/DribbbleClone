6d491e4530fbfc7241fc1e110edb0f02f6a0eeb1
Nooshin <2011dragonmails@gmail.com>
2024-05-17 22:25:09 +0330

    load more data completed

--- script/index.js
+++ script/index.js
@@ -5,39 +5,55 @@ const artList = document.querySelector(".art-list");
 const artTemplate = document.getElementById("art-info");
 const loading = document.querySelector(".loading-container");
 const main = document.querySelector(".main");
+const loadMoreData = document.getElementById("load-more-data");
+let pageNumber = 1;
 
-const loadData = () => {
-  let artData = {};
-  fetch("https://api.artic.edu/api/v1/artworks?page=1&limit=9")
-    .then((response) => response.json())
-    .then((data) => {
-      loading.classList.add("hidden");
-      artData = data.data;
-      // console.log(artData[2]);
-      // console.log(artData[3]);
-      if (Object.keys(artData).length > 0) {
-        main.classList.remove("hidden");
+const fetchData = (page) => {
+  return fetch(`https://api.artic.edu/api/v1/artworks?page=${page}&limit=12`)
+    .then((response) => {
+      if (response.status >= 200 && response.status < 300) {
+        return response.json();
+      } else {
+        return response.json.then((errData) => {
+          console.log(errData);
+          throw new Error("Something went Wrong - server-side.");
+        });
       }
-      artData.forEach((element) => {
-        const artBody = document.importNode(artTemplate.content, true);
-        artBody.querySelector(
-          "img"
-        ).src = `https://www.artic.edu/iiif/2/${element.image_id}/full/843,/0/default.jpg`;
-        artBody.querySelector(".artist").textContent = element.artist_title;
-        artBody.querySelector(".img-desc").textContent = element.title;
-        artBody.querySelector(".likes").textContent = Math.floor(
-          Math.random() * 100
-        ).toString();
-        artBody.querySelector(".views").textContent =
-          (Math.floor(Math.random() * (1000 - 100) + 100) / 100).toString() +
-          "k";
-        artList.append(artBody);
-      });
     })
     .catch((error) => {
       console.log(error);
+      throw new Error("Something went wrong!");
     });
 };
+
+async function fetchArtWorks() {
+  try {
+    loadMoreData.disabled = true;
+    loadMoreData.textContent = "Loading...";
+    const responseData = await fetchData(pageNumber + 1);
+    const listOfArtworks = responseData.data;
+    loading.classList.add("hidden");
+    pageNumber = pageNumber + 1;
+    listOfArtworks.forEach((element) => {
+      const artBody = document.importNode(artTemplate.content, true);
+      artBody.querySelector(
+        "img"
+      ).src = `https://www.artic.edu/iiif/2/${element.image_id}/full/843,/0/default.jpg`;
+      artBody.querySelector(".artist").textContent = element.artist_title;
+      artBody.querySelector(".img-desc").textContent = element.title;
+      artBody.querySelector(".likes").textContent = Math.floor(
+        Math.random() * 100
+      ).toString();
+      artBody.querySelector(".views").textContent =
+        (Math.floor(Math.random() * (1000 - 100) + 100) / 100).toString() + "k";
+      artList.append(artBody);
+      loadMoreData.disabled = false;
+      loadMoreData.textContent = "Load More ArtWork";
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+}
 
 const mobileNavHandler = (e) => {
   const navItem = e.target.tagName === "H3" ? e.target : null;
@@ -71,6 +87,8 @@ const burgerMenuHandler = () => {
   }
 };
 
-loadData();
+fetchArtWorks();
+
 mobileNav.addEventListener("click", mobileNavHandler);
 burger.addEventListener("click", burgerMenuHandler);
+loadMoreData.addEventListener("click", fetchArtWorks);
